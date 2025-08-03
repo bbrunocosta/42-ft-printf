0a2e5f893c1db2675df03bc540cd6af0fecae0b0
Bruno Costa Borges <bcosta-b@student.42sp.org.br>
2025-08-03 16:32:57 -0300

    Add ft_memcpy implementation

---
+++ libs/memft/ft_memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcosta-b <bcosta-b@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 16:32:57 by bcosta-b          #+#    #+#             */
+/*   Updated: 2025/08/03 16:39:12 by bcosta-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	d = dest;
+	s = src;
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
