614b218c331d9d2c1c72d371b2d9acb53a586dbb
Bruno Costa Borges <bcosta-b@student.42sp.org.br>
2025-08-05 12:54:45 -0300

    Add charft public header

---
+++ libs/charft/charft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   charft.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bcosta-b <bcosta-b@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 12:54:45 by bcosta-b          #+#    #+#             */
+/*   Updated: 2025/08/05 13:00:15 by bcosta-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARFT_H
+# define CHARFT_H
+
+# include <unistd.h>
+
+ssize_t	ft_putchar(int c);
+int		ft_isdigit(int c);
+int		ft_isspace(int c);
+
+#endif
